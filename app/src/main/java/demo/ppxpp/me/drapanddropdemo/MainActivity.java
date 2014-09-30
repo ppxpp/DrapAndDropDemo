43bcd551e3e53f1c66424bcbb6cc7894b32baf71
zhouhao <zhouhao3207@qq.com>
2014-09-30 15:38:38 +0800

    aaaaaaa

--- app/src/main/java/demo/ppxpp/me/drapanddropdemo/MainActivity.java
+++ app/src/main/java/demo/ppxpp/me/drapanddropdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.ClipDescription;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.DragEvent;
@@ -53,7 +54,7 @@ public class MainActivity extends Activity implements View.OnLongClickListener {
     @Override
     public boolean onLongClick(View v) {
         ClipData.Item item = new ClipData.Item(((TextView) v).getText().toString());
-        ClipData data = new ClipData("label", new String[]{"text/plain"}, item);
+        ClipData data = new ClipData("label", new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN}, item);
         View.DragShadowBuilder dsb = new View.DragShadowBuilder(v);
         v.startDrag(data, dsb, null, 0);
         return true;
@@ -73,39 +74,37 @@ public class MainActivity extends Activity implements View.OnLongClickListener {
                     break;
                 case DragEvent.ACTION_DRAG_ENTERED:
                     Log.d("ACTION_DRAG_STARTED", "ACTION_DRAG_ENTERED return true for " + v);
-                    /*AnimatorSet set = new AnimatorSet();
+                    AnimatorSet set = new AnimatorSet();
                     ObjectAnimator animator1 = ObjectAnimator.ofInt(v, "left", v.getLeft(), v.getLeft() - 30);
                     ObjectAnimator animator2 = ObjectAnimator.ofInt(v, "right", v.getRight(), v.getRight() + 30);
                     set.play(animator1).with(animator2);
                     set.setDuration(500);
-                    set.start();*/
-                    v.setLeft(v.getLeft() - 30);
-                    v.setRight(v.getRight() + 30);
+                    set.start();
                     return true;
                 case DragEvent.ACTION_DRAG_LOCATION:
                     Log.d("ACTION_DRAG_STARTED", "ACTION_DRAG_LOCATION return true for " + v);
                     return true;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    /*AnimatorSet set1 = new AnimatorSet();
+                    AnimatorSet set1 = new AnimatorSet();
                     ObjectAnimator animator3 = ObjectAnimator.ofInt(v, "left", v.getLeft(), v.getLeft() + 30);
                     ObjectAnimator animator4 = ObjectAnimator.ofInt(v, "right", v.getRight(), v.getRight() - 30);
                     set1.play(animator4).with(animator3);
                     set1.setDuration(500);
-                    set1.start();*/
-                    v.setLeft(v.getLeft()+30);
-                    v.setRight(v.getRight() - 30);
+                    set1.start();
+                    /*v.setLeft(v.getLeft() + 30);
+                    v.setRight(v.getRight() - 30);*/
                     Log.d("ACTION_DRAG_STARTED", "ACTION_DRAG_EXITED return true for " + v);
                     return true;
                 case DragEvent.ACTION_DROP:
                     Log.d("ACTION_DRAG_STARTED", "ACTION_DROP return true for " + v);
-                    /*AnimatorSet set2 = new AnimatorSet();
+                    AnimatorSet set2 = new AnimatorSet();
                     ObjectAnimator animator33 = ObjectAnimator.ofInt(v, "left", v.getLeft(), v.getLeft() + 30);
                     ObjectAnimator animator44 = ObjectAnimator.ofInt(v, "right", v.getRight(), v.getRight() - 30);
                     set2.play(animator44).with(animator33);
                     set2.setDuration(500);
-                    set2.start();*/
-                    v.setLeft(v.getLeft() + 30);
-                    v.setRight(v.getRight() - 30);
+                    set2.start();
+                    /*v.setLeft(v.getLeft() + 30);
+                    v.setRight(v.getRight() - 30);*/
                     ClipData.Item item = event.getClipData().getItemAt(0);
                     //btn1.setText(btn1.getText().toString() + item.getText());
                     ((TextView)v).setText(item.getText() + "_" + flag++);
